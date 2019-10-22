2321387f6e5708927021cd326eeeca9fa10986b5
wukrit <sukritwalia@gmail.com>
2019-10-21 20:07:05 -0400

    added 5 word prompts for easy testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # => Seeds
 
 50.times do
+    arr_of_words = Prompt.fetch(5)
+    Prompt.create(text: arr_of_words.join(' '), length: 5)
+end
+
+50.times do
     arr_of_words = Prompt.fetch(50)
     Prompt.create(text: arr_of_words.join(' '), length: 50)
 end
