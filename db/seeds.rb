cc7625ff1d4d048af4a09738402a1e44f8c4146b
wukrit <sukritwalia@gmail.com>
2019-10-23 23:53:34 -0400

    fixed prompt generator seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
 # => Seeds
 
-100.times do
+500.times do
     arr_of_words = Prompt.generate(5)
     Prompt.create(text: arr_of_words.join(' '), length: 5)
 end
 
-100.times do
-    arr_of_words = Prompt.generate(50)
+500.times do
+    arr_of_words = Prompt.generate(25)
     Prompt.create(text: arr_of_words.join(' '), length: 25)
 end
 
-100.times do
+500.times do
     arr_of_words = Prompt.generate(50)
     Prompt.create(text: arr_of_words.join(' '), length: 50)
 end
 
-100.times do
-    arr_of_words = Prompt.generate(50)
+500.times do
+    arr_of_words = Prompt.generate(75)
     Prompt.create(text: arr_of_words.join(' '), length: 75)
 end
 
-100.times do
+500.times do
     arr_of_words = Prompt.generate(100)
     Prompt.create(text: arr_of_words.join(' '), length: 100)
 end
